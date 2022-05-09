dfc0a28a7a7582785857d8a948978327b1a45784
Jacob Jordan <31334963+Naturealizor@users.noreply.github.com>
2022-05-09 13:30:53 -0400

    copied over front page so far

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div />
+
+<header>
+	<img src="images/stationary logo 3.jpg" alt="cradle logo" />
+	<img src="images/Group_63.png" alt="cradle logo" />
+
+	<ul>
+		<li><a href="">About Us</a></li>
+		<li><a href="">Gallery</a></li>
+		<li><a href="">Contact Us</a></li>
+	</ul>
+</header>
+<main>
+	<img src="images/Cavernet-Sauvignon-Grapes-on-the-vine 1.jpg" alt="grape vine" />
+</main>
+
+<style>
+	main {
+		width: 100%;
+	}
+</style>
